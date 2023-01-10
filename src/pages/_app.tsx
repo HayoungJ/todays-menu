9fac57405cd2fe2983d23a67de835473bb9d95c7
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-10 21:34:44 +0900

    ADD: styled components 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/global-style';
+import { theme } from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>오늘 뭐 먹지</title>
+        <meta
+          name="description"
+          content="오늘 먹을 메뉴를 정하는데 도움을 주는 애플리케이션"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
