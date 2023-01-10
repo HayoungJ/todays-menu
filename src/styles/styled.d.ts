39b32f2ff8240bb2b538bacc5d45935bce08ac7f
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-10 21:36:23 +0900

    ADD: global & theme 스타일과 그 타입 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,20 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    fontSize: {
+      xs: string;
+      sm: string;
+      base: string;
+      md: string;
+      lg: string;
+    };
+
+    palette: {
+      red: string;
+      gray: string;
+    };
+
+    borderRadius: string;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from 'styled-components';
+
+const fontSize = {
+  xs: '0.5rem',
+  sm: '0.75rem',
+  base: '1rem',
+  md: '1.25rem',
+  lg: '1.5rem',
+};
+
+const palette = {
+  red: '#dd2b1f',
+  gray: '#dedede',
+};
+
+export const theme: DefaultTheme = {
+  fontSize,
+  palette,
+  borderRadius: '4px',
+};
