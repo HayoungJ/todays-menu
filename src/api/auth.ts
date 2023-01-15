f2c36e973e323777896db31eaa14508b52508ab1
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-15 23:49:47 +0900

    ADD: firebase에 구글 로그인/로그아웃을 요청하는 api 추가

---
+++ src/api/auth.ts
@@ -0,0 +1,11 @@
+import { auth } from '../lib/firebase';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+export const googleLogin = () => {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(auth, provider);
+};
+
+export const googleLogout = () => {
+  return signOut(auth);
+};
