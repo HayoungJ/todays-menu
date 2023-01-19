11c4fddc40c7109cf04774dc1da879a1d94e0090
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-19 23:01:44 +0900

    FIX: firebase 관련 타입 커스텀 타입 파일이 아닌 직접 불러오도록 수정
    - 커스텀 타입 파일을 생성해 보관한다고 해도 어차피 import는 필요
    - 본래 목적인 '혼동 없는 이름으로 사용'하는 것은 import 하며 re-name해 쓰기로 함

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,23 +1,21 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
 import { auth } from '../lib/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-
-import { firebaseUser } from '../types/user';
+import { onAuthStateChanged, User as IFirebaseUser } from 'firebase/auth';
 
 import { getCookie, setCookie } from '../utils/cookie';
 
-const UserContext = createContext<firebaseUser | null>(null);
+const UserContext = createContext<IFirebaseUser | null>(null);
 
 export const UserProvider = ({ children }: any) => {
-  const [user, setUser] = useState<firebaseUser | null>(null);
+  const [user, setUser] = useState<IFirebaseUser | null>(null);
 
   const token = getCookie('token');
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(
       auth,
-      async (user: firebaseUser | null) => {
+      async (user: IFirebaseUser | null) => {
         if (!user) return;
 
         setUser(user);
