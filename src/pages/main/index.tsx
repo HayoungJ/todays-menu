81864dba2cef5389ebf13792330eb895478dba93
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-19 23:04:16 +0900

    ADD: 유저 메뉴와 관련된 상수 추가 ADD: token이 없으면 로그인 페이지로 리디렉션 처리
    - 상수들은 나중에 별도의 파일로 관리할 예정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,8 +1,32 @@
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { googleLogout } from '../../api/auth';
 import HeaderTemplate from '../../components/templates/HeaderTemplate';
 
+import { useUser } from '../../contexts/UserContext';
+import { deleteCookie, getCookie } from '../../utils/cookie';
+
 function MainPage() {
-  const locationOptions = ['회사'];
+  const router = useRouter();
+
+  const token = getCookie('token');
+
+  const locationOptions = ['회사', '집'];
+  const userMenus = [
+    {
+      label: '계정 설정',
+      onClick: () => {},
+    },
+    {
+      label: '로그아웃',
+      onClick: async () => {
+        await googleLogout();
+        deleteCookie('token');
+      },
+    },
+  ];
+
+  const user = useUser();
 
   const [location, setLocation] = useState(locationOptions[0]);
 
@@ -10,11 +34,18 @@ function MainPage() {
     setLocation(option);
   };
 
+  useEffect(() => {
+    // todo: 사용자가 사용 중인 상태에서 (다른 페이지 로그인 등으로) 쿠키에 토큰이 추가되었을 때는 자동 확인이 되지 않음
+    if (!token) router.push('/login');
+  }, []);
+
   return (
     <HeaderTemplate
       logoURL="/assets/images/text_logo.png"
+      profileURL={user?.photoURL ?? 'https://via.placeholder.com/80'}
       location={location}
       locationOptions={locationOptions}
+      userMenus={userMenus}
       handleLocationSelect={(option, index) => onLocationSelect(option, index)}
     />
   );
