b9007eb664560cf7a372224cb844ba218daafdd4
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-19 22:59:21 +0900

    ADD: 유저 메뉴 추가 FIX: 장소 선택 select box 위치 조정

--- src/components/templates/HeaderTemplate.tsx
+++ src/components/templates/HeaderTemplate.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import SelectBox from '../atoms/SelectBox';
 
 import styled from 'styled-components';
+import UserMenu from '../organisms/UserMenu';
+import { IMenu } from '../../types/types';
 
 const StyledHeader = styled.header`
   display: flex;
@@ -15,28 +17,38 @@ const StyledHeader = styled.header`
   padding: 0 1.5rem;
 `;
 
+const LocationSelectBox = styled(SelectBox)`
+  margin-left: auto;
+  margin-right: 2.2rem;
+`;
+
 interface IHeaderTemplate {
   logoURL: string;
+  profileURL: string;
   location: string;
   locationOptions: string[];
+  userMenus: IMenu[];
   handleLocationSelect: (option: string, index: number) => any;
 }
 
 function HeaderTemplate({
   logoURL,
+  profileURL,
   location,
   locationOptions,
+  userMenus,
   handleLocationSelect,
 }: IHeaderTemplate) {
   return (
     <StyledHeader>
       <Image src={logoURL} alt="logo" width={160} height={80} />
-      <SelectBox
+      <LocationSelectBox
         width={10}
         label={location}
         options={locationOptions}
         handleSelect={handleLocationSelect}
       />
+      <UserMenu profileURL={profileURL} userMenus={userMenus} />
     </StyledHeader>
   );
 }
