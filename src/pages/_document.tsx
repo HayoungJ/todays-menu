be6d818c75e97299cb9b0fee8835bdacf6f5dbf7
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-19 15:29:55 +0900

    ADD: modal Portal을 위한 element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,12 @@ class MyDocument extends Document {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            sheet.collectStyles(
+              <>
+                <App {...props} />
+                <div id="modal-root" />
+              </>
+            ),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
