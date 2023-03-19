a3de7cc2414c33693a77460aa85638a86593b1e8
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-19 15:27:56 +0900

    ADD: 전역 모달

--- src/components/atoms/BaseModal.tsx
+++ src/components/atoms/BaseModal.tsx
@@ -1,6 +1,10 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
 import styled, { css } from 'styled-components';
+import { useModal } from '../../contexts/ModalContext';
+
+import BaseButton from './BaseButton';
 
 const StyledModalWrap = styled.div`
   width: 100vw;
@@ -10,7 +14,7 @@ const StyledModalWrap = styled.div`
   top: 0;
   left: 0;
 
-  z-index: 10;
+  z-index: 20;
 `;
 
 const StyledModalBackground = styled.div`
@@ -55,24 +59,65 @@ const ModalTitle = styled.section``
 
 const ModalContent = styled.section``
 
-const ModalFooter = styled.section``
+const ModalFooter = styled.section`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: end;
 
-interface IModal {
+  padding: 0.7rem;
+  margin-top: auto;
+
+  button {
+    margin-left: 10px;
+  }
+`;
+
+interface IBaseModal {
   Title: ReactNode;
   Content: ReactNode;
-  Footer: ReactNode;
 }
 
-const BaseModal: FC<IModal> = ({ Title, Content, Footer, ...props }) => {
+const BaseModal: FC<IBaseModal> = ({ Title, Content, ...props }) => {
+  const { isOpen, closeModal, modalDispatch } = useModal();
+  const { onCancel, onSubmit } = modalDispatch;
+
   return (
-    <StyledModalWrap {...props}>
-      <StyledModalBackground />
-      <StyledModal>
-        <ModalTitle>{ Title }</ModalTitle>
-        <ModalContent>{ Content }</ModalContent>
-        <ModalFooter>{ Footer }</ModalFooter>
-      </StyledModal>
-    </StyledModalWrap>
+    <>
+      { isOpen && createPortal(
+        <StyledModalWrap {...props}>
+          <StyledModalBackground />
+          <StyledModal>
+            <ModalTitle>{ Title }</ModalTitle>
+            <ModalContent>{ Content }</ModalContent>
+            <ModalFooter>
+              { onCancel && onSubmit ?  
+                <>
+                  <BaseButton
+                    label="취소"
+                    color="gray"
+                    width={10}
+                    onClick={onCancel}
+                  />
+                  <BaseButton
+                    label="등록"
+                    color="red"
+                    width={10}
+                    onClick={onSubmit}
+                  />
+                </> :
+                <BaseButton
+                  label="닫기"
+                  color="red"
+                  width={10}
+                  onClick={closeModal}
+                />
+              }
+            </ModalFooter>
+          </StyledModal>
+        </StyledModalWrap>,
+        document.querySelector('#modal-root') as HTMLDivElement
+      )}
+    </>
   );
 }
   
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,13 +1,13 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect, FC, ReactNode } from 'react';
 
-import { auth } from '../lib/firebase';
+import { auth } from '../lib/Firebase';
 import { onAuthStateChanged, User as IFirebaseUser } from 'firebase/auth';
 
 import { deleteCookie, setCookie } from '../utils/cookie';
 
 const UserContext = createContext<IFirebaseUser | null>(null);
 
-export const UserProvider = ({ children }: any) => {
+export const UserProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<IFirebaseUser | null>(null);
 
   useEffect(() => {
