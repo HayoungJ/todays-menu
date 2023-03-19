c1c1ac74bf53f25925e179a4a1b0654ca64219aa
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-19 20:49:21 +0900

    ADD: 위치 추가 모달, 식당 등록 모달

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -6,6 +6,7 @@ import HeaderTemplate from '../../components/templates/HeaderTemplate';
 import PickerTemplate from '../../components/templates/PickerTemplate';
 import LocationList from '../../components/organisms/LocationList';
 import BaseModal from '../../components/atoms/BaseModal';
+import MealForm from '../../components/organisms/MealForm';
 
 import { googleLogout } from '../../api/auth';
 import { getLocationByKeyword } from '../../api/map';
@@ -13,9 +14,10 @@ import { getLocationByKeyword } from '../../api/map';
 import { useUser } from '../../contexts/UserContext';
 import { useModal } from '../../contexts/ModalContext';
 
-import { ISearchResult } from '../../types/types';
+import { ILocation, IMeal } from '../../types/types';
 
 import styled, { css } from 'styled-components';
+import { createUserLocation, createLocationMeal, deleteUserLocation, getLocationMeals, getUserLocations } from '../../api/common';
 
 const StyledMain = styled.main`
   display: flex;
@@ -49,12 +51,12 @@ const ModalContent = styled.div`
 
 const MainPage: FC = () => {
   const router = useRouter();
-  const locationOptions = ['회사', '집'];
   const userMenus = [
     {
       label: '위치 등록',
       onClick: () => {
-        openModal({
+        openLocationModal({
+          id: 'location',
           onClose: () => {
             setSearchResult([]);
             setSelectedResult(null);
@@ -72,20 +74,41 @@ const MainPage: FC = () => {
   ];
 
   const user = useUser();
-  const { isOpen, openModal } = useModal();
+  const { openModal: openLocationModal } = useModal();
+  const { openModal: openMealModal } = useModal();
 
-  const [location, setLocation] = useState(locationOptions[0]);
-  const [searchResult, setSearchResult] = useState<ISearchResult[]>([]);
-  const [selectedResult, setSelectedResult] = useState<ISearchResult | null>(null);
+  const [locations, setLocations] = useState<ILocation[]>([]);
+  const [location, setLocation] = useState<ILocation>({
+    name: '',
+    address: '',
+    latitude: '',
+    longitude: '',
+  });
+  const [searchResult, setSearchResult] = useState<ILocation[]>([]);
+  const [selectedResult, setSelectedResult] = useState<ILocation | null>(null);
+  const [meals, setMeals] = useState<IMeal[]>([]);
+  const [meal, setMeal] = useState<IMeal | null>(null);
+  const [newMeal, setNewMeal] = useState<IMeal>({
+    name: '',
+    category: '',
+    address: '',
+    foods: [{
+      food: '',
+      price: '',
+    }],
+    far: 0,
+  });
 
   const onLocationSelect = (option: string, index: number) => {
-    setLocation(option);
+    setLocation(locations[index]);
   };
 
-  const handleSubmit = (name: string, address: string) => {
-    console.log(name, address);
+  const handleSubmit = async (result: ILocation) => {
+    if (!user?.uid) return;
+    await createUserLocation(user.uid, result);
     setSearchResult([]);
     setSelectedResult(null);
+    await getLocations(user.uid);
   }
 
   const handleSearch = async (keyword: string) => {
@@ -106,34 +129,145 @@ const MainPage: FC = () => {
     setSearchResult(result);
   }
 
-  const handleSearchSelect = (value: ISearchResult) => {
+  const handleSearchSelect = (value: ILocation) => {
     setSelectedResult(value);
   }
+
+  const handleDelete = async (location: ILocation) => {
+    if (!user?.uid) return;
+    const locationId = location?.id;
+    if (!locationId) return;
+    await deleteUserLocation(user.uid, locationId);
+    await getLocations(user.uid);
+  }
+
+  const getLocations = async (uid: string) => {
+    const locations = await getUserLocations(uid).then((data) => {
+      return data ? Object.keys(data).map((id) => {
+        return {
+          id,
+          ...data[id]
+        }
+      }) : [];
+    });
+    setLocations(locations);
+  }
+
+  const getMeals = async (locationId: string) => {
+    const meals = await getLocationMeals(locationId).then((data) => {
+      return data ? Object.values(data) : [];
+    }) as IMeal[];
+    setMeals(meals);
+  }
+
+  const onMealCancel = () => {
+
+  }
+
+  const onMealSubmit = () => {
+    if (!location?.id) return;
+    createLocationMeal(location.id, newMeal);
+    setNewMeal({
+      name: '',
+      category: '',
+      address: '',
+      foods: [{
+        food: '',
+        price: '',
+      }],
+      far: 0,
+    });
+  }
+
+  const handleMealChange = (value: string | { food: string, price: string }[], key: 'name' | 'category' | 'address' | 'foods') => {
+    setNewMeal({
+      ...newMeal,
+      [key]: value,
+    });
+  }
+
+  useEffect(() => {
+    if (!user?.uid) return;
+    const fetchData = async () => {
+      await getLocations(user.uid);
+    }
+    fetchData();
+  }, [user]);
+
+  useEffect(() => {
+    if (locations.length === 0) {
+      setLocation({
+        name: '',
+        address: '',
+        latitude: '',
+        longitude: '',
+      });
+      return;
+    }
+    setLocation(locations[0]);
+  }, [locations]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!location?.id) return;
+      await getMeals(location.id);
+    }
+    fetchData();
+  }, [location]);
+
+  useEffect(() => {
+    const length = meals.length;
+    if (length === 0) return;
+    const index = Math.round(Math.random() * (length - 1));
+    setMeal(meals[index]);
+  }, [meals]);
 
   return (
     <StyledMain>
       <HeaderTemplate
         logoURL="/assets/images/text_logo.png"
         profileURL={user?.photoURL ?? 'https://via.placeholder.com/80'}
-        location={location}
-        locationOptions={locationOptions}
+        location={location.name}
+        locationOptions={locations.map((location) => location.name)}
         userMenus={userMenus}
         handleLocationSelect={(option, index) =>
           onLocationSelect(option, index)
         }
       />
-      <StyledPicker />
+      <StyledPicker
+        meals={meals}
+        meal={meal}
+        handleAdd={() => openMealModal({ 
+          id: 'meal',
+          onCancel: () => onMealCancel,
+          onSubmit: () => onMealSubmit(),
+        })}
+      />
       <BaseModal
+        id="location"
         Title={<ModalTitle>위치 등록</ModalTitle>}
         Content={
           <ModalContent>
             <LocationList
-              locations={[]}
+              locations={locations}
               searchResult={searchResult}
               selectedResult={selectedResult}
-              handleSumbit={handleSubmit}
+              handleSubmit={handleSubmit}
               handleSearch={handleSearch}
               handleSearchSelect={handleSearchSelect}
+              handleDelete={handleDelete}
+            />
+          </ModalContent>
+        }
+      />
+      <BaseModal
+        id="meal"
+        Title={<ModalTitle>식당 등록</ModalTitle>}
+        Content={
+          <ModalContent>
+            <MealForm
+              meal={newMeal}
+              handleMealChange={handleMealChange}
             />
           </ModalContent>
         }
